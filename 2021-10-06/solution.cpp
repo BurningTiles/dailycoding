b4e42d5632041d50ff69e81a55ed52fa77da0a9c
Manish Kumawat <BurningTiles>
2021-10-07 11:47:21 +0530

    Updated

--- 2021-10-06/solution.cpp
+++ 2021-10-06/solution.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 string longest_common_prefix(vector<string> v){
 	if (v.size() == 0) return "";
-    string prefix = v[0];
-    for (int i=1; i<v.size(); i++){
+	string prefix = v[0];
+	for (int i=1; i<v.size(); i++){
 		int j=0;
 		while(j<v[i].size() && j<prefix.size() && v[i][j]==prefix[j]) j++;
 		if(j==0) return "";
 		prefix = prefix.substr(0,j);
-	}    
-    return prefix;
+	}
+	return prefix;
 }
 
 signed main() {
